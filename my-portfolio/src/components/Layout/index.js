7d1ee6ce1603d22b9ee77393444580610713219f
Patrick <patrickbastosc@gmail.com>
2022-06-13 19:20:35 -0300

    feat: Created Sidebar component

--- my-portfolio/src/components/Layout/index.js
+++ my-portfolio/src/components/Layout/index.js
@@ -1,5 +1,8 @@
+import Sidebar from '../Sidebar';
 import './index.scss'
 
 export default function Layout() {
-  return <>Hello</>
+  return <>
+    <Sidebar />
+  </>;
 }
---
+++ my-portfolio/src/components/Sidebar/index.js
@@ -0,0 +1,70 @@
+import { Link, NavLink } from 'react-router-dom';
+import './index.scss';
+import LogoS from '../../assets/images/logo-s.png';
+import LogoSubtitle from '../../assets/images/logo_sub.png';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+    faHome,
+    faUser,
+    faEnvelope,
+  } from '@fortawesome/free-solid-svg-icons';
+
+import {
+  FaGithub,
+  FaLinkedin
+} from "react-icons/fa";
+
+
+
+export default function Sidebar() {
+  return (
+    <div className="nav-bar">
+      <Link className="logo" to="/">
+        <img src={ LogoS } alt='logo' />
+        <img className="sub-logo"src={ LogoSubtitle } alt="slogodan" />        
+      </Link>
+      <nav>
+        <NavLink
+          exact="true"
+          activeclassname="active"
+          to="/">
+          <FontAwesomeIcon icon={faHome} color="#4d4d4e" />
+        </NavLink>
+        <NavLink
+          exact="true"
+          activeclassname="active"
+          className="about-link"
+          to="/about">
+          <FontAwesomeIcon icon={faUser} color="#4d4d4e" />
+        </NavLink>
+        <NavLink
+          exact="true"
+          activeclassname="active"
+          className="contact-link"
+          to="/contact">
+          <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
+        </NavLink>
+      </nav>
+      <ul>
+        <li>
+          <a
+            target="_blank"
+            rel="noreferrer"
+            href="https://github.com/PatrickBastosDeveloper"
+          >
+            <FaGithub size={30} />
+          </a>
+        </li>
+        <li>
+          <a
+            target="_blank"
+            rel="noreferrer"
+            href="https://www.linkedin.com/in/patrickbastosdeveloper/"
+          >
+            <FaLinkedin size={ 30 } />
+          </a>
+        </li>
+      </ul>
+    </div>
+  )
+}
