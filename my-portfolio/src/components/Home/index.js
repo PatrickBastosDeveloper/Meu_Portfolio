c02c9bf04a03fa3a970e8eb59a761fbc4de9331a
Patrick <patrickbastosc@gmail.com>
2022-06-17 17:36:06 -0300

    fix: Personal name on home page

--- my-portfolio/src/components/Home/index.js
+++ my-portfolio/src/components/Home/index.js
@@ -1,6 +1,6 @@
 import { useEffect, useState} from 'react';
 import { Link } from "react-router-dom";
-import LogoTitle from '../../assets/images/logo-s.png';
+// import LogoTitle from '../../assets/images/logo-s.png';
 // import Logo from '../Logo';
 import './index.scss';
 import AnimatedLetters from '../AnimatedLetters'
@@ -8,7 +8,7 @@ import Loader from 'react-loaders';
 
 export default function Home() {
   const [letterClass, setLetterClass] = useState('text-animate');
-  const nameArray = ['P','a','t','r','i','c','k',' ','B','a','s','t','o','s'];
+const nameArray = ['','P','a','t','r','i','c','k',' ','B','a','s','t','o','s'];
   const jobArray = ['w','e','b',' ','d','e','v','e','l','o','p','e','r','.'];
   
   useEffect(() => {
@@ -23,11 +23,11 @@ export default function Home() {
       <div className="text-zone">
         <h1>
           <span className={ letterClass }>H</span>
-          <span className={`${letterClass}_12`}>i,</span>
-          <br />
-          <span className={`${letterClass}_13`}>I</span>
-          <span className={`${letterClass}_14`}>'M</span>
-        <img src={ LogoTitle } alt="developer" />
+          <span className={`${letterClass} _12`}>i,</span>
+            <br />
+            <span className={`${letterClass} _13`}>I</span>
+            <span className={`${letterClass} _14`}>'m</span>
+        {/* <img src={ LogoTitle } alt="developer" /> */}
           <AnimatedLetters
             letterClass={ letterClass }
             strArray={ nameArray }
@@ -45,7 +45,7 @@ export default function Home() {
       </div>
       {/* <Logo /> */}
     </div>
-      <Loader type="pacman"/>
+    <Loader type="cube-transition"/>
     </>
   )
 }
