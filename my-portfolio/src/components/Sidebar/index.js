99dd4f72b276b8168fa378743160bf1b5de0ca52
Patrick <patrickbastosc@gmail.com>
2022-06-17 17:35:09 -0300

    feat: Remove img test

--- my-portfolio/src/components/Sidebar/index.js
+++ my-portfolio/src/components/Sidebar/index.js
@@ -1,12 +1,13 @@
 import { Link, NavLink } from 'react-router-dom';
 import './index.scss';
-import LogoS from '../../assets/images/logo-s.png';
-import LogoSubtitle from '../../assets/images/logo_sub.png';
+// import LogoS from '../../assets/images/logo-s.png';
+// import LogoSubtitle from '../../assets/images/logo_sub.png';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {
     faHome,
     faUser,
     faEnvelope,
+    faSuitcase
   } from '@fortawesome/free-solid-svg-icons';
 
 import {
@@ -20,8 +21,8 @@ export default function Sidebar() {
   return (
     <div className="nav-bar">
       <Link className="logo" to="/">
-        <img src={ LogoS } alt='logo' />
-        <img className="sub-logo"src={ LogoSubtitle } alt="slogodan" />        
+        {/* <img src={ LogoS } alt='logo' /> */}
+        {/* <img className="sub-logo"src={ LogoSubtitle } alt="slogodan" /> */}
       </Link>
       <nav>
         <NavLink
@@ -36,6 +37,13 @@ export default function Sidebar() {
           className="about-link"
           to="/about">
           <FontAwesomeIcon icon={faUser} color="#4d4d4e" />
+        </NavLink>
+        <NavLink
+          exact="true"
+          activeclassname="active"
+          className="portfolio-link"
+          to="/portfolio">
+          <FontAwesomeIcon icon={faSuitcase} color="#4d4d4e" />
         </NavLink>
         <NavLink
           exact="true"
