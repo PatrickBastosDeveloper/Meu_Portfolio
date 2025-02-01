7a2d133b251b3caf1e06e514d9324e8848333f81
PatrickBastosDeveloper <patrickbastosc@gmail.com>
2025-02-01 17:18:38 -0300

    Changed config vite

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,11 @@
-import {defineConfig } from "vite";
+import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-//https://vitejs.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react()],
-    base: "/my-portfolio"
+  plugins: [react()],
+  base: "/my-portfolio/",
+  build: {
+    outDir: "build", // Define a pasta de saída correta
+  },
 });
