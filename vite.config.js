32f159947ce111f1044768b56e330057e8be28e9
PatrickBastosDeveloper <patrickbastosc@gmail.com>
2025-02-08 12:52:58 -0300

    Move index to try deploy

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,9 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/my-portfolio/",
-  build: {
-    outDir: "/build", // Define a pasta de saída correta
-    assetsDir: "static",
-  },
+  base: "my-portfolio"
+  // build: {
+  //   outDir: "/build", // Define a pasta de saída correta
+  //   assetsDir: "static",
+  // },
 });
